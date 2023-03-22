4ca870e7d0b7ac904ad77eed6711a0314fed5680
pranay <pranayanand61@gmail.com>
2023-03-22 16:09:40 +0530

    pru commit

--- src/main/java/com/niit/jap/Main.java
+++ src/main/java/com/niit/jap/Main.java
@@ -2,6 +2,24 @@ package com.niit.jap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double number1 = 50.79;
+        int number2 = 15;
+        System.out.println("number1" + " = " + number1);
+        System.out.println("number2" + " = " + number2);
+        double addition = number1 + number2;
+        System.out.println("50.79 + 15" + " = " + addition);
+        double subtraction = number1 - number2;
+        System.out.println("50.79 - 15" + " = " + subtraction);
+        double multiplication = number1 * number2;
+        System.out.println("50.79 * 15" + " = " + multiplication);
+        double division = number1 / number2;
+        System.out.println("50.79 / 15" + " = " + division);
+        double modulus = number1 % number2;
+        System.out.println("50.79 % 15" + " = " + modulus);
     }
 }
+
+
+
+
+
